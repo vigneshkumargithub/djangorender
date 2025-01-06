a65cfffaa659a4f2fd6e31cf741f9fadee0e8ed8
vigneshkumargithub <133019918+vigneshkumargithub@users.noreply.github.com>
2025-01-06 12:43:48 +0530

    Update middleware.py

--- apps/corecode/middleware.py
+++ apps/corecode/middleware.py
@@ -1,17 +1,43 @@
-from .models import AcademicSession, AcademicTerm
+# from .models import AcademicSession, AcademicTerm
 
+
+# class SiteWideConfigs:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
+
+#     def __call__(self, request):
+#         current_session = AcademicSession.objects.get(current=True)
+#         current_term = AcademicTerm.objects.get(current=True)
+
+#         request.current_session = current_session
+#         request.current_term = current_term
+
+#         response = self.get_response(request)
+
+#         return response
+
+
+### new
+
+from django.core.exceptions import ObjectDoesNotExist
+from .models import AcademicSession, AcademicTerm
 
 class SiteWideConfigs:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        current_session = AcademicSession.objects.get(current=True)
-        current_term = AcademicTerm.objects.get(current=True)
+        try:
+            current_session = AcademicSession.objects.get(current=True)
+            request.current_session = current_session
+        except ObjectDoesNotExist:
+            request.current_session = None
 
-        request.current_session = current_session
-        request.current_term = current_term
+        try:
+            current_term = AcademicTerm.objects.get(current=True)
+            request.current_term = current_term
+        except ObjectDoesNotExist:
+            request.current_term = None
 
         response = self.get_response(request)
-
         return response
